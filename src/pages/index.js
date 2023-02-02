3033fa5bf594434fa3dabdcb4e4ce759ea2ae817
naga <yuto7687@gmail.com>
2023-02-03 00:03:59 +0900

    ✨ Card コンポーネントを作成

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import styles from '@/styles/Home.module.css'
 import Banner from 'components/Banner'
 import Header from 'components/Header'
+import LargeCard from 'components/LargeCard'
+import MediumCard from 'components/MediumCard'
+import SmallCard from 'components/Smallcard'
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({ exploreData,cardsData }) {
   return (
     <div className=''>
       <Head>
@@ -16,8 +19,40 @@ export default function Home() {
 
       <main className='max-w-7xl mx-auto px-8 sm:px-16'>
         <section className='pt-6'>
-          <h2 className='h-screen text-4xl font-semibold pd-5'>Explore Nearby</h2>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
           {/* pull some data from server - API endpoints */}
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            { exploreData?.map(({img, distance, location}) => (
+              <SmallCard
+                key={img}
+                img={img}
+                distance={distance}
+                location={location}
+              />
+            ))}
+          </div>
+        </section>
+
+        <section>
+          <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+          <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
+            { cardsData?.map(({img, title }) => (
+              <MediumCard 
+                key={img} 
+                img={img}
+                title={title}
+              />
+            ))}
+          </div>
+        </section>
+
+        <section>
+          <LargeCard 
+            img='https://links.papareact.com/4cj'
+            title="The Greatest Outdoors"
+            description="Wishlists curated by Airbnb."
+            buttonText="Get Inspired"
+          />
         </section>
       </main>
     </div>
@@ -25,5 +60,21 @@ export default function Home() {
 }
 
 export async function getStaticProps(){
-  
+  const exploreData = await fetch('https://www.jsonkeeper.com/b/4G1G').
+  then(
+    (res) => res.json()
+  );
+
+  const cardsData = await await fetch('https://www.jsonkeeper.com/b/VHHT').
+  then((res) =>
+  res.json()
+  );
+
+
+  return {
+    props: {
+      exploreData,
+      cardsData,
+    },
+  };
 }
