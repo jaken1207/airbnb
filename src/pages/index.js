85823f2aef91d74c9a471de66afb522e483c1e68
naga <yuto7687@gmail.com>
2023-02-02 21:29:28 +0900

    ✨ Banner コンポーネントを追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import styles from '@/styles/Home.module.css'
+import Banner from 'components/Banner'
 import Header from 'components/Header'
 import Head from 'next/head'
 
@@ -11,7 +12,18 @@ export default function Home() {
       </Head>
       
       <Header />
-      {/* Banner */}
+      <Banner />
+
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='h-screen text-4xl font-semibold pd-5'>Explore Nearby</h2>
+          {/* pull some data from server - API endpoints */}
+        </section>
+      </main>
     </div>
   )
+}
+
+export async function getStaticProps(){
+  
 }
