aaece9cde000edbced24fbc9e64d9fbd4a354510
naga <yuto7687@gmail.com>
2023-02-04 02:19:35 +0900

    ✨ 検索カレンダーの追加

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-5 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
+    <div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
         <div className='space-y-4 text-xs text-gray-800'>
             <h5 className='font-bold'>ABOUT</h5>
             <p>How Airbnb works</p>
@@ -18,13 +18,6 @@ function Footer() {
             <p>This is not a real site</p>
             <p>Its a pretty awesome clone</p>
             <p>Referrals accepted</p>
-        </div>
-
-         <div className='space-y-4 text-xs text-gray-800'>
-            <h5 className='font-bold'>SUPPORT</h5>
-            <p>Help Centre</p>
-            <p>Trust & Safety</p>
-            <p>And so on</p>
         </div>
 
          <div className='space-y-4 text-xs text-gray-800'>
