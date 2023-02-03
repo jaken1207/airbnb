bf7b94fbc31645940a942eb4721a8806b5a992c0
naga <yuto7687@gmail.com>
2023-02-04 02:35:55 +0900

    ✏️ フッターの数を変更

--- components/Footer.js
+++ components/Footer.js
@@ -21,12 +21,10 @@ function Footer() {
         </div>
 
          <div className='space-y-4 text-xs text-gray-800'>
-            <h5 className='font-bold'>ABOUT</h5>
-            <p>How Airbnb works</p>
-            <p>Newsroom</p>
-            <p>Investors</p>
-            <p>Airbnb Plus</p>
-            <p>Airbnb Luxe</p>
+            <h5 className='font-bold'>SUPPORT</h5>
+            <p>Heop Centre</p>
+            <p>Trust & Safety</p>
+            <p>And so on</p>
         </div>
         
         <div className='space-y-4 text-xs text-gray-800'>
